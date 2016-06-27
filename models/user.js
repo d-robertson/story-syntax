2a6ad6ad7983f4a200f0d77fd70d89bd0dd1b263
Chris Madison <CKMadison4@gmail.com>
2016-06-27 10:10:48 -0700

    Added associations for the story and user models. Next steps are to setup post route for a story, and associate them with a user's session.

--- models/user.js
+++ models/user.js
@@ -33,7 +33,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        models.user.hasMany(models.story);
       }
     },
     instanceMethods: {
